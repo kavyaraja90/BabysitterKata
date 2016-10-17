b2385158dffc9d58d070dc4466fbd11a1209c17e
kavya <kavyaraja.90@gmail.com>
2016-10-17 18:19:13 -0400

    adding more tests, functionality to valid job timings

--- src/main/java/com/babysitting/BabysittingJob.java
+++ src/main/java/com/babysitting/BabysittingJob.java
@@ -9,6 +9,9 @@ public class BabysittingJob {
 	public static final int DEFAULT_BEDTIME = 21;
 
 	public static final String START_TIME_UNAVAILABLE_MESSAGE = "Please specify a start time after 5PM and before 4AM";
+	public static final String END_TIME_INVALID_MESSAGE = "Please specify an end time, "
+			+ "greater than the start time and before 4AM";
+	public static final String BEDTIME_INVALID_MESSAGE = "Bedtime needs to be between the start time and end time";
 
 	int startTime;
 	int endTime;
@@ -31,10 +34,22 @@ public class BabysittingJob {
 	private void validateTimeSlot(int startTime, int endTime, int bedTime) {
 		if (startTime < EARLIEST_START_TIME && startTime > LATEST_END_TIME) {
 			throw new NotAvailableException(START_TIME_UNAVAILABLE_MESSAGE);
+		} else if (endTime < EARLIEST_START_TIME && endTime > LATEST_END_TIME) {
+			throw new NotAvailableException(END_TIME_INVALID_MESSAGE);
+		} else if (bedTime < startTime || bedTime > endTime) {
+			throw new NotAvailableException(BEDTIME_INVALID_MESSAGE);		
 		}
 	}
 	
 	public int getStartTime() {
 		return startTime;
+	}
+
+	public Object getEndTime() {
+		return endTime;
+	}
+
+	public Object getBedtime() {
+		return bedTime;
 	}
 }
--- src/test/java/com/babysitting/BabysittingJobTest.java
+++ src/test/java/com/babysitting/BabysittingJobTest.java
@@ -12,5 +12,17 @@ public class BabysittingJobTest {
         new BabysittingJob(12, 2, BabysittingJob.DEFAULT_BEDTIME);
     }
     
+    @Test(expected = NotAvailableException.class)
+    public void babySitterDoesNotAcceptInvalidEndTimeAfter4AM() {
+    	new BabysittingJob(12,5, BabysittingJob.DEFAULT_BEDTIME);  	
+    }
+    
+    @Test
+    public void babySittingJobIsCreatedWhenValidStartAndEndTimesAreSpecified(){
+    	BabysittingJob job = new BabysittingJob(18, 3, 20);
+    	assertEquals(18, job.getStartTime());
+    	assertEquals(3, job.getEndTime());
+    	assertEquals(20, job.getBedtime());	
+    }
 
 }
